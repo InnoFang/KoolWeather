9b00dc9a3a708ff93b4a57c751545fc86c846722
InnoFang <q984941707@qq.com>
2017-07-02 17:31:23 +0800

    update UI

--- app/src/main/java/io/innofang/koolweather/ui/activities/WeatherActivity.kt
+++ app/src/main/java/io/innofang/koolweather/ui/activities/WeatherActivity.kt
@@ -2,6 +2,7 @@ package io.innofang.koolweather.ui.activities
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
@@ -46,6 +47,11 @@ class WeatherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= 21) {
+            window.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        }
+
         setContentView(R.layout.activity_weather)
 
         val prefs = PreferenceManager.getDefaultSharedPreferences(WeatherActivity@ this)
